ff1daeba4e0a60aacda10493c26c6115d132698b
arkanafaisal <arkana.faisal.r@gmail.com>
2026-06-04 16:37:12 +0700

    delete: unused files & router config

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -6,8 +6,6 @@ import { lazy, Suspense } from "react"
 import { Loader2 } from "lucide-react"
 
 const NotFoundPage = lazy(() => import("@/pages/not-found"))
-// const LoginPage = lazy(() => import("@/pages/auth/login"))
-// const RegisterPage = lazy(() => import("@/pages/auth/register"))
 const DashboardPage = lazy(() => import("@/pages/dashboard"))
 const ProductListPage = lazy(() => import("@/pages/products/product-list"))
 const ProductDetailPage = lazy(() => import("@/pages/products/product-detail"))
@@ -18,25 +16,9 @@ const StoreFormPage = lazy(() => import("@/pages/stores/store-form"))
 const StoreVisitPage = lazy(() => import("@/pages/stores/store-visit"))
 const FinancePage = lazy(() => import("@/pages/finance.tsx"))
 const JourneyPage = lazy(() => import("./pages/journey"))
-// const InvoiceListPage = lazy(() => import("@/pages/finance/invoice-list"))
-// const InvoiceDetailPage = lazy(() => import("@/pages/finance/invoice-detail"))
-// const ReceivablesPage = lazy(() => import("@/pages/finance/receivables"))
-// const StorePerformancePage = lazy(() => import("@/pages/reports/store-performance"))
-// const ItemTrackingPage = lazy(() => import("@/pages/reports/item-tracking"))
-// const FinancialReportPage = lazy(() => import("@/pages/reports/financial-report"))
-// const ProfilePage = lazy(() => import("@/pages/settings/profile"))
-// const PreferencesPage = lazy(() => import("@/pages/settings/preferences"))
 const SettingsPage = lazy(() => import("@/pages/settings"))
 
 export const router = createBrowserRouter([
-  // {
-  //   path: "/login",
-  //   element: <LoginPage />,
-  // },
-  // {
-  //   path: "/register",
-  //   element: <RegisterPage />,
-  // },
   {
     path: "/journey",
     element: (
@@ -64,14 +46,6 @@ export const router = createBrowserRouter([
       { path: "stores/:id/edit", element: <StoreFormPage /> },
       { path: "stores/:id/visit", element: <StoreVisitPage /> },
       { path: "finance", element: <FinancePage /> },
-      // { path: "finance/invoices", element: <InvoiceListPage /> },
-      // { path: "finance/invoices/:id", element: <InvoiceDetailPage /> },
-      // { path: "finance/receivables", element: <ReceivablesPage /> },
-      // { path: "reports/stores", element: <StorePerformancePage /> },
-      // { path: "reports/tracking", element: <ItemTrackingPage /> },
-      // { path: "reports/financial", element: <FinancialReportPage /> },
-      // { path: "settings/profile", element: <ProfilePage /> },
-      // { path: "settings/preferences", element: <PreferencesPage /> },
       { path: "settings", element: <SettingsPage /> },
 
       { path: "*", element: <NotFoundPage /> },
