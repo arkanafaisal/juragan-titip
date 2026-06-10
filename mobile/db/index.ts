536ca03d5d7dca50442a5f2af1e90dde93087e19
arkanafaisal <arkana.faisal.r@gmail.com>
2026-06-10 21:47:30 +0700

    update(db): migration

--- mobile/db/index.ts
+++ mobile/db/index.ts
@@ -2,6 +2,6 @@ import { openDatabaseSync } from 'expo-sqlite';
 import { drizzle } from 'drizzle-orm/expo-sqlite';
 import * as schema from './schema';
 
-const expoDb = openDatabaseSync('juragantitip.db');
+const expoDb = openDatabaseSync('juragantitip.db_temp');
 
 export const db = drizzle(expoDb, { schema });
---
+++ mobile/drizzle/0001_dusty_sandman.sql
@@ -0,0 +1,1 @@
+CREATE UNIQUE INDEX `products_normalized_name_unique` ON `products` (`normalized_name`);
