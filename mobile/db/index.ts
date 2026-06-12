388646c5c0e21b7ce8d8e0e49c7b5e231a972624
arkanafaisal <arkana.faisal.r@gmail.com>
2026-06-12 22:49:55 +0700

    update(db): register store table

--- mobile/db/index.ts
+++ mobile/db/index.ts
@@ -3,7 +3,18 @@ import { drizzle } from 'drizzle-orm/expo-sqlite';
 import { products } from './schema/products.schema';
 import { inventoryLogs } from './schema/inventory-logs.schema';
 import { stores } from './schema/stores.schema';
+import { visits, visitItems, visitsRelations, visitItemsRelations } from './schema/visits.schema';
 
 const expoDb = openDatabaseSync('juragantitip.db');
 
-export const db = drizzle(expoDb, { schema: { products, inventoryLogs, stores } });
+export const db = drizzle(expoDb, { 
+  schema: { 
+    products, 
+    inventoryLogs, 
+    stores,
+    visits,
+    visitItems,
+    visitsRelations,
+    visitItemsRelations
+  } 
+});
